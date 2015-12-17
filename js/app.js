6a1cc09f9f2c7e091c2ac6d532dd80ac6834a634
MehdiHenna <mehdihennaoui@gmail.com>
2015-12-17 17:34:00 +0100

    mise en place du mustache et du .hide() en jquery

--- js/app.js
+++ js/app.js
@@ -1,8 +1,32 @@
+$(document).ready(function(){
 
-
-$.getJSON( "http://vps227573.ovh.net/u-06.json", function(data) { 
 	
-	console.log(data);
+
+	var url = "http://vps227573.ovh.net/u-06.json";
+
+	
+	$.getJSON(url, function(data){
+
+		var template = $('#template').html();
+		
+		Mustache.parse(template);	
+		
+		var rendered  = Mustache.render(template, data);
+
+		$('body').html(rendered);		
+
+	});
+
+	$('body').on("click", "a", function(){
+
+		$(".texte").hide();
+
+		var id = $(this).attr('href');
+
+		$(id).toggle();
+	
+	});
+
 
 });
 
